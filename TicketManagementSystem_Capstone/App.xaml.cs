95b45f3b952ae65ad9497b3b9143b6b16e50fcdd
Branden Alder <brandenalder121@gmail.com>
2024-04-09 16:45:07 -0500

    Added Generic host package, added new main.

--- TicketManagementSystem_Capstone/App.xaml.cs
+++ TicketManagementSystem_Capstone/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using Microsoft.Extensions.Hosting;
+using TicketManagementSystem_Capstone.View;
 
 namespace TicketManagementSystem_Capstone
 {
@@ -9,9 +11,29 @@ namespace TicketManagementSystem_Capstone
     /// </summary>
     public partial class App : Application
     {
-        public App()
+
+        [STAThread]
+        public static void Main(string[]? args)
         {
-            
+            using IHost host = CreateHostBuilder(args).Build();
+            host.Start();
+            App app = new();
+            app.InitializeComponent();
+            app.Run();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[]? args)
+        {
+           return Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+
+                });
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
         }
     }
 
